7bab05cbbd03a7d076c5378ef4b45c4ce5933345
sanderweg <140747509+sanderweg@users.noreply.github.com>
2026-04-03 11:43:17 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,11 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/fireba
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 import { getDatabase, ref, push, set, remove, onValue, get } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "JOUW_API_KEY",
-  authDomain: "JOUW_PROJECT.firebaseapp.com",
-  databaseURL: "https://JOUW_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "JOUW_PROJECT",
+export const firebaseConfig = {
+  apiKey: "AIzaSyAHJ_U7_H7rO1CLDEmgYm2bY-956R2B3jI",
+  authDomain: "karting-competitie.firebaseapp.com",
+  databaseURL: "https://karting-competitie-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "karting-competitie",
+  storageBucket: "karting-competitie.firebasestorage.app",
+  messagingSenderId: "915335846004",
+  appId: "1:915335846004:web:5fbc6592f60a93a9031921"
 };
 
 const app = initializeApp(firebaseConfig);
