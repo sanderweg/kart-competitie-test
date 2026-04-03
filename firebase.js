a5bf2999370714defa3666010d6c0b84da1e7512
sanderweg <140747509+sanderweg@users.noreply.github.com>
2026-04-03 12:13:09 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getDatabase, ref, push, set, remove, onValue, get } from "https://www.g
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAHJ_U7_H7rO1CLDEmgYm2bY-956R2B3jI",
-  authDomain: "karting-competitie.firebaseapp.com",
-  databaseURL: "https://karting-competitie-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "karting-competitie",
-  storageBucket: "karting-competitie.firebasestorage.app",
-  messagingSenderId: "915335846004",
-  appId: "1:915335846004:web:5fbc6592f60a93a9031921"
+  apiKey: "AIzaSyAMkgqa_PrSGPwBhDZtljxJZbVbwHq_qCQ",
+  authDomain: "karting-competitie-26804.firebaseapp.com",
+  databaseURL: "https://karting-competitie-26804-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "karting-competitie-26804",
+  storageBucket: "karting-competitie-26804.firebasestorage.app",
+  messagingSenderId: "144090410040",
+  appId: "1:144090410040:web:6f9b22645926da62f78acd"
 };
 
 export const DB_PATH = "kartCompetitie/races";
